7796d8f0eed9d72eef0a6a840eb5d91d519e91de
oipi <tuberosumgames@gmail.com>
2025-02-23 12:41:58 +0530

    added pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,20 @@
 import React, { useState } from "react";
+import { Route,Routes } from "react-router-dom";
+
+import Quiz from "./pages/Quiz";
+import LeaderBoard from "./pages/LeaderBoard";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-
+return(
+    <div style={{ minHeight: '100vh' }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Quiz />} />
+        <Route path="/leaderboard" element={<LeaderBoard />} />
+      </Routes>
+    </div>
+)
 };
 
 export default App;
